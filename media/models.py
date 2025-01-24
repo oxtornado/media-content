bda67d257af7460cef710cf3aae9621ba4407426
oxtornado <dnielussa@gmail.com>
2025-01-23 21:58:46 -0500

    imagenes reformadas para el tamaño

--- media/models.py
+++ media/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+# agregamos valores default para 
+# que no tengamos que hacerlo 
+# despues de hacer las migraciones:
+
 class MediaContent(models.Model):
-    link = models.URLField()  # Enlace al contenido
-    poster = models.URLField()  # Póster del contenido
-    title_image = models.URLField()  # Imagen PNG del título
-    transition_image = models.URLField()  # Imagen PNG de transición
+    link = models.URLField()  # Enlace al contenido aqui si no ponemos valor default porque cuando migramos ya estaba definido
+    poster = models.URLField(default="https://via.placeholder.com/300")  # Póster del contenido
+    title_image = models.URLField(default="https://via.placeholder.com/150x50")  # Imagen PNG del título
+    transition_image = models.URLField(default="https://via.placeholder.com/150")  # Imagen PNG de transición
 
     def __str__(self):
-        return self.link  # Representación textual del modelo
+        return self.link
